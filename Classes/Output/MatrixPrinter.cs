222b3c19c47af8c2597a6a572f36747b81dff846
twix20 <leereek@gmail.com>
2020-07-02 14:58:40 +0200

    feat(MatrixPrinter): LampMatrix ToString implementation

---
+++ Classes/Output/MatrixPrinter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using BerlinClock.Classes.BusinessLogic;
+
+namespace BerlinClock.Classes.Output
+{
+    public class MatrixPrinter : IMatrixPrinter
+    {
+        private const string NewLine = "\r\n";
+
+        public string ToString(LampMatrix matrix)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var rowLamps in matrix.Rows)
+            {
+                foreach (var lamp in rowLamps)
+                {
+                    sb.Append(lamp.LampSign);
+                }
+
+                sb.Append(NewLine);
+            }
+
+            sb.Length -= NewLine.Length;
+
+            return sb.ToString();
+        }
+    }
+}
