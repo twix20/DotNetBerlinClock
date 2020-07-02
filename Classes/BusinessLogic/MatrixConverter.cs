3b634a0a68a2a59967c8df6f53b74705b4d2ebf5
twix20 <leereek@gmail.com>
2020-07-02 14:56:20 +0200

    feat(MatrixConverter): FromTimeSpan implementation

--- Classes/BusinessLogic/MatrixConverter.cs
+++ Classes/BusinessLogic/MatrixConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BerlinClock.Classes.BusinessLogic.Lamps;
+using System;
 
 namespace BerlinClock.Classes.BusinessLogic
 {
@@ -6,7 +7,33 @@ namespace BerlinClock.Classes.BusinessLogic
     {
         public LampMatrix FromTimeSpan(TimeSpan time)
         {
-            throw new NotImplementedException();
+            var matrix = new LampMatrix();
+            matrix.OddSecondsLamp.Enabled = IsBlinking(time);
+
+            foreach (var rowLamps in matrix.RowsExceptOddSecondsLamp)
+            {
+                foreach (var lamp in rowLamps)
+                {
+                    if (ShouldEnableLamp(time, lamp))
+                    {
+                        time = EnableLamp(time, lamp);
+                    }
+                }
+            }
+
+            return matrix;
         }
+
+        private static TimeSpan EnableLamp(TimeSpan time, Lamp lamp)
+        {
+            time -= lamp.Time;
+            lamp.Enabled = true;
+
+            return time;
+        }
+
+        private static bool ShouldEnableLamp(TimeSpan time, Lamp lamp) => time - lamp.Time >= TimeSpan.Zero;
+
+        private static bool IsBlinking(TimeSpan time) => time.Seconds % 2 == 0;
     }
 }
